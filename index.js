d249dd254021de97d3270daf5ab0676aef373fbd
drew-sarette <103331340+drew-sarette@users.noreply.github.com>
2022-07-23 11:01:48 -0400

    Fix layout and simplify CSS

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 document.getElementById("main-menu-button").addEventListener("click", toggleMenu);
-document.querySelectorAll(".content-item").forEach(element => {
-    element.addEventListener("click", toggleMenuItem(element));
-});
 
 function toggleMenu() {
     document.querySelector(".content-area").classList.toggle("show-main-menu");
