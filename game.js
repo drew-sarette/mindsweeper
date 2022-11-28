b9cfee71baef25740da4ea0f65c9ec6d576c8397
Drew Sarette <103331340+drew-sarette@users.noreply.github.com>
2022-11-27 19:24:16 -0500

    Add memory mode, flagging feature, update some styles

--- game.js
+++ game.js
@@ -1,34 +1,40 @@
 /* Global Variables */
 const mindsweeper = [];
-let hardMode = true;
+let memoryModeCheckBox = document.getElementById('memory-mode');
+let memoryMode = memoryModeCheckBox.checked;
 const gameArea = document.getElementById('game-area');
 const startBtn = document.getElementById("start");
 const sizeInput = document.getElementById('size-input')
 
+
 startBtn.addEventListener("click", initialize);
+memoryModeCheckBox.addEventListener('click', toggleMemoryMode);
+
+function toggleMemoryMode() {
+  memoryMode = memoryModeCheckBox.checked;
+}
+
 
 /* Create an array with a boolean for each div, representing the presence of a mine. Set a click-event listener for each square.  */
 function initialize() {
   const size = Number(sizeInput.value);
-  while(mindsweeper.length) mindsweeper.pop();
+  while (mindsweeper.length) mindsweeper.pop();
   gameArea.innerHTML = null;
   gameArea.style.gridTemplateColumns = "repeat(" + size + ", 40px)";
   gameArea.style.gridTemplateRows = "repeat(" + size + ", 40px)";
-  for (let row = 0; row < size; row++){
-    mindsweeper.push([]) 
-    for(let col = 0; col < size; col++){
+  for (let row = 0; row < size; row++) {
+    mindsweeper.push([])
+    for (let col = 0; col < size; col++) {
       mindsweeper[row].push(makeSquare(row, col));
       gameArea.appendChild(mindsweeper[row][col].div);
     }
   }
-  console.log(mindsweeper);
 }
 
 function makeSquare(row, col) {
   const div = document.createElement('div');
   div.classList.add('game-square');
-  div.addEventListener('mousedown', () => handleMove(row, col));
-  return {
+  const square =  {
     div: div,
     row: row,
     col: col,
@@ -36,67 +42,83 @@ function makeSquare(row, col) {
     clicked: false,
     flagged: false
   }
+  div.addEventListener('mousedown', () => handleMouseDown(square));
+  return square;
 }
 
 /* Check if the clicked square was a mine, then apply the appropriate CSS class. */
-function handleMove(row, col) {
-  const square = mindsweeper[row][col];
-  let longClick = false;
-  setTimeout(() => { longClick = true }, 400);
-  square.div.addEventListener('mouseup', () => {
-    if (longClick) {
-      if (square.flagged)  {
-        square.flagged = false;
-        square.div.classList.remove('flagged');
-        square.div.textContent = null;
-      }
-      square.flagged = true;
-      square.div.textContent = 'F';
-      square.div.classList.add('flagged');
-    }
-    else {
-      if (square.isMine) {
-        youLose();
-      }
-      else {
-        square.div.textContent = countAdjacentMines(row, col);
-        if (hardMode) {
-          setTimeout(() => { square.div.textContent = null}, 2000);
-        }
-        if (youWon()) {
-          const winnerObj = {
-            name: prompt('Congratulations!  Enter your name to post your score.'),
-            gameState: mindsweeper
-          }
-          Object.freeze(winnerObj);
-        }
-      }
-    }
-  })
-  
+function handleMouseDown(square) {
+  square.div.addEventListener('mouseup', function handleMouseup() {
+    clearTimeout(timeoutID1);
+    if (!square.clicked && !square.flagged) checkMove(square);
+    square.div.removeEventListener('mouseup', handleMouseup);
+  });
+  const timeoutID1 = setTimeout(() => { 
+    toggleFlagState(square); 
+  }, 400);
+}
 
+function checkMove(square) {
+  [row, col] = [square.row, square.col];
+  if (square.isMine) {
+    youLose();
+  }
+  else {
+    square.div.classList.add('safe');
+    square.clicked = true;
+    square.div.textContent = countAdjacentMines(row, col);
+    if (memoryMode) {
+      const timeoutID2 = setTimeout(() => { square.div.textContent = null }, 2000);
+    }
+    if (youWon()) {
+      const winnerObj = {
+        name: prompt('Congratulations!  Enter your name to post your score.'),
+        gameState: mindsweeper
+      }
+      Object.freeze(winnerObj);
+    }
+  }
+}
+
+function toggleFlagState(square) {
+  if (square.flagged) {
+    square.flagged = false;
+    square.clicked = false;
+    square.div.classList.remove('flagged');
+    square.div.textContent = null;
+  } else {
+    if (square.clicked) return;
+    square.flagged = true;
+    square.div.textContent = 'F';
+    square.div.classList.add('flagged');
+  }
 }
 
 function youWon() {
   let youWon = true;
-  for (row = 0; row < mindsweeper.length; row ++){
-    for (col = 0; col < mindsweeper.length; col++){
+  for (row = 0; row < mindsweeper.length; row++) {
+    for (col = 0; col < mindsweeper.length; col++) {
       if (!mindsweeper[row][col].isMine && !mindsweeper[row][col].clicked) {
         youWon = false;
       }
     }
   }
-  return youWon; 
+  return youWon;
 }
 
+
 function youLose() {
-  for (row = 0; row < mindsweeper.length; row ++){
-    for (col = 0; col < mindsweeper.length; col++){
+
+  for (row = 0; row < mindsweeper.length; row++) {
+    for (col = 0; col < mindsweeper.length; col++) {
       const square = mindsweeper[row][col];
-      if (square.isMine) {
+      if (square.isMine && square.flagged) {
+        break;
+      }
+      else if (square.isMine) {
         square.div.classList.add('mine');
       }
-      else if (!square.clicked ) {
+      else if (!square.clicked) {
         square.div.classList.add('safe');
         square.div.textContent = countAdjacentMines(row, col);
       }
@@ -109,12 +131,12 @@ function countAdjacentMines(row, col) {
   const adjacentCoordinates = [
     [row - 1, col - 1], [row - 1, col], [row - 1, col + 1],
     [row, col - 1], [row, col + 1],
-    [row + 1, col -1], [row + 1, col], [row + 1, col +1]
+    [row + 1, col - 1], [row + 1, col], [row + 1, col + 1]
   ]
   const size = mindsweeper.length;
-adjacentCoordinates.forEach(([row, col]) => {
-  if (row < 0 || col < 0 || row >= size || col >= size) return;
-  if (mindsweeper[row][col].isMine) adjacentMinesCounter++;
+  adjacentCoordinates.forEach(([row, col]) => {
+    if (row < 0 || col < 0 || row >= size || col >= size) return;
+    if (mindsweeper[row][col].isMine) adjacentMinesCounter++;
   });
   return adjacentMinesCounter;
 }
